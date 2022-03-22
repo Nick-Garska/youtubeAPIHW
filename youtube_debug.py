662d5c4079dcf973224b33409bcbfa00ab02dced
Nick <ngarska@emich.edu>
2022-03-22 17:01:05 -0400

    added loop to run search 5 times

--- youtube_debug.py
+++ youtube_debug.py
@@ -15,20 +15,7 @@ def youtubeSearch(queryTerm, max_results, page_token=None):
         q=queryTerm,
         part= "id,snippet",
         maxResults=max_results,
-        pageToken=page_token 
     ).execute()
-
-    videos =[]
-
-    for searchResult in searchResponce["items"]:
-        if searchResult ["id"]["kind"] == "youtube#video":
-            videos.append(searchResult)
-
-        #assumes we have more than 1 page
-        return videos, searchResponce["nextPageToken"]
-
-
-
 
     return searchResponce
 
@@ -36,15 +23,8 @@ if __name__ == "__main__":
     queryTerm =sys.argv[1]
     max_results = sys.argv[2]
     try:
-        
-
-
-        for i in range(5): 
-            videos, nextPageToken = youtubeSearch(queryTerm, max_results)
-            videos, nextPageToken = youtubeSearch(queryTerm, max_results, nextPageToken)
-
-        print(videos)
-
+        response = youtubeSearch(queryTerm, max_results)
+        print(response)
     except HttpError as e:
             print("An Http error %d occured: /n%s" % (type(e).__name__, str(e)))
 
